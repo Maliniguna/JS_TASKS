f95a2d2295bcc55899b422f6d47dcccd044a0231
Maliniguna <maliniguna186@gmail.com>
2024-08-17 20:12:04 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,61 @@
+//Chp2-ts-1
+var score;
+var playerName;
+var level;
+//Chp2-ts-2
+var score;
+score = 200;
+console.log(score);
+var score;
+score = 250;
+var score2;
+score2 = 150;
+var score;
+score = 200;
+console.log(score); // This will display 200
+var score2;
+score2 = 150;
+console.log(score2); // This will display 150
+//Chp2-ts-5
+var message;
+message = "Hello World!";
+console.log(message); 
+message = 'Congratulations!';
+console.log(message); 
+//Chp2-ts-6
+var playerName;
+var locationName;
+var score;
+playerName = "Lionel Messi";
+locationName = "Argentina";
+score = 100;
+console.log(playerName + " is in " + locationName);
+console.log(playerName + " has a score of " + score);
+//Chp2-ts-8
+var score;
+score = 100;
+console.log("Your score was " + score);
+console.log("Great splat!!!");
+score = score + 50;
+console.log("New score: " + score);
+console.log("Way to go!");
+console.log("You successfully splatted a kumquat!");
+score = score + 100;
+console.log("New score after splatting: " + score);
+console.log("Congratulations! You're doing great!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
